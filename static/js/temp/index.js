36321fad39fcd553ac85ae116cf3e9f0332fd068
wb-ymf342847 <wb-ymf342847@alibaba-inc.com>
2018-01-12 17:32:26 +0800

    my test code

--- static/js/temp/index.js
+++ static/js/temp/index.js
@@ -1,5 +1,5 @@
 var myApp = angular.module("voteApp", []);
-myApp.controller("myCtrl", function($scope) {
+myApp.controller("myCtrl", function($scope,$compile) {
     $scope.firstName = "Neo";
     $scope.lastName = "Huang";
 
@@ -14,19 +14,44 @@ myApp.controller("myCtrl", function($scope) {
 //                console.log(angular.element('#actionListBox'));
 //                console.log($("#actionListBox").find('div').remove());
 //                $document.find("actionListBox").find('div').remove()
-        var template =
-            "<ul id='myDropdown' class=\"dropdown-menu\" style=\"width:275px;height:200px;collapse: red;\">" +
-            "<li ng-repeat=\"spa in actions\" ng-click=\"selectMobilePage($index);\"><a href=\"javascript:;\"></a></li>" +
-            "</ul>";
+        var template ="<ul id='myDropdown' class=\"dropdown-menu1\" style=\"width:100px;height:100px;\">"+
+        "<li ng-repeat=\"aa in actions\" ng-click=\"selectAction("+index+");\">" +
+        "{{aa.code}}" +
+        "</li>" +
+        "</ul>";
+        /*var actions=$scope.actions;
+        
+        actions.forEach(function (val,index) {
+            // template += "<li ng-click=\"selectAction("+index+");\">";
+            template += "<li ng-click=\"addAction();\">";
+            template += "<a href=\"javascript:;\" class=\"ng-binding\">"+val.code+"</a>";
+            template += "</li>";
+        })*/
 
+        // template+="</ul>";
+        // $compile(template)($scope)
         // $("#actionListBox").find('dl')[index].find('div:last').append("template");
-        $("#actionListBox").find('dl:last').find('div:last').parent("div").append(template);
+        $("#actionListBox").find('dl:last').find('div:last').parent("div").append($compile(template)($scope));
         $("#myDropdown").show();
-        console.log("bingo");
+
+
+
+        console.log("point "+$("#myDropdown").length);
 
 
     }
 
+
+
+    $scope.selectAction=function () {
+        $("#myDropdown").remove();
+        console.log("1234151234");
+        $scope.actions.push({
+            code:'aa',
+            name:'vv',
+            nameEn:'cc'
+        });
+    }
 
     $scope.addAction=function () {
 
